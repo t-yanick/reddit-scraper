b0378fadbe3a47d076d8bcf7fa1a9ad0c752283b
Yanick <tyankee15@gmail.com>
2020-09-24 17:46:58 -0300

    added .rubocop.yml file

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,16 +8,28 @@ def scraper
     parsed_page = Nokogiri::HTML(unparsed_page)
     jobs = Array.new
     job_listings = parsed_page.css('div.jobsearch-SerpJobCard') #15 jobs on first page
-    job_listings.each do |job_listing|
-        job = {
-            title: job_listing.css('a.jobtitle').text,
-            company: job_listing.css('span.company').text,
-            location: job_listing.css('span.location').text,
-            url: "https://indeed.com" + job_listing.css('a')[0].attributes["href"].value
-        }
-        jobs << job
+    page = 1
+    per_page = job_listings.count #15
+    total = parsed_page.css('div.searchCountContainer').text.split(' ')[3].gsub(',','').to_i #6484
+    last_page = (total.to_f / per_page.to_f ).round #432
+    while page <= last_page
+        pagination_url = "https://www.indeed.com/jobs?q=software+developer&l=Remote&start=#{page}"
+        pagination_unparsed_page = HTTParty.get(pagination_url)
+        pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page)
+        pagination_job_listings = pagination_parsed_page.css('div.jobsearch-SerpJobCard')
+        pagination_job_listings.each do |job_listing|
+            job = {
+                title: job_listing.css('a.jobtitle').text,
+                company: job_listing.css('span.company').text,
+                location: job_listing.css('span.location').text,
+                url: "https://indeed.com" + job_listing.css('a')[0].attributes["href"].value
+            }
+            jobs << job
+        end
+        page +=
     end
     byebug
 end
 
 scraper
+
