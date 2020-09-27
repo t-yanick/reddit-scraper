7b13ed98a50b747ef7d846f62cf733ebe4bbb51c
Yanick <tyankee15@gmail.com>
2020-09-27 09:20:09 -0300

    created method to open files scraped from website

---
+++ lib/results.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class Results
+  attr_reader :file
+
+  def initialize(arr)
+    prefix = '.remote_jobs/job_listing_for_'
+    @file = File.open(prefix + "#{arr[0]}.html", 'w+')
+  end
+
+  def begin_html
+    File.open('./html/begin.txt').each do |line|
+      @file.puts line
+    end
+  end
+
+  def save(title_arr, company_arr, location_arr, url_arr)
+    h3 = '<h3></h3>'
+    h4 = '<h4></h4>'
+    line = '.............................................'
+    (0..title_arr.length).each do |x|
+      @file.puts "#{h3}#{title_arr[x]}#{h4}#{company_arr[x]}#{h4}#{location_arr[x]}#{h4}#{url_arr[x]}#{h4}#{line[x]}"
+    end
+  end
+
+  def end_html
+    File.open('./html/end.txt').each do |line|
+      @file.puts line
+    end
+  end
+
+  def close_file
+    @file.close
+  end
+end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,66 +1,68 @@
+# frozen_string_literal: true
+
 require 'byebug'
 require 'httparty'
 require 'nokogiri'
 
 class Scraper
-    attr_reader :job, :jobs, :total, :page, :titles, :company, :locations, :urls, :parsed_page, :pagination 
-    
-    private
+  attr_reader :job, :jobs, :total, :page, :titles, :company, :locations, :urls, :parsed_page, :pagination
 
-    def initialize
-        @titles = []
-        @company = []
-        @locations =[]
-        @urls = []
-    end
+  private
 
-    public
+  def initialize
+    @titles = []
+    @company = []
+    @locations = []
+    @urls = []
+  end
 
-    def scraper (arr)
-        url = 'https://www.indeed.com/jobs?q=&l=Remote'
-        unparsed_page = HTTParty.get(url)
-        parsed_page = Nokogiri::HTML(unparsed_page.body)
-        jobs = Array.new
-        job_listings = parsed_page.css('div.jobsearch-SerpJobCard') #15 jobs on first page
-        page = 0
-        per_page = job_listings.count #15
-        total = parsed_page.css('div.searchCountContainer').text.split(' ')[3].gsub(',','').to_i #6484
-        last_page = (total.to_f / per_page.to_f ).round #432
-        while page <= last_page
-            # pagination_url = "https://www.indeed.com/jobs?q=software+developer&l=Remote&start=#{page}"
-            pagination_url = "https://www.indeed.com/jobs?q=&l=Remote&start=#{page}"
+  public
 
-            puts pagination_url 
-            puts "Page: #{page}"
-            puts ''
-            pagination_unparsed_page = HTTParty.get(pagination_url)
-            pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page.body)
-            pagination_job_listings = pagination_parsed_page.css('div.jobsearch-SerpJobCard')
-            pagination_job_listings.each do |job_listing|
-                job = {
-                    title: job_listing.css('a.jobtitle').children[0].text.strip,
-                    # title: job_listing.css('a.jobtitle').text.strip.partition('/n')[0],
-                    # company: job_listing.css('span.company').text,
-                    # location: job_listing.css('span.location').text,
-                    # url: "https://indeed.com" + job_listing.css('a')[0].attributes["href"].value
-                }
-                if arr.all? { |x| job[:title].downcase.split.include?(x) }
-                    @titles << (job[:title])
-                    # @company << (@job[:company])
-                    # @locations << (@job[:location])
-                    # @urls << (@job[:url])
-                end
-            
-                jobs << job
-                puts @titles
-            end
-            page += 10
-          #byebug
+  def scraper(arr)
+    url = 'https://www.indeed.com/jobs?q=&l=Remote'
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page.body)
+    jobs = []
+    job_listings = parsed_page.css('div.jobsearch-SerpJobCard') # 15 jobs on first page
+    page = 0
+    per_page = job_listings.count # 15
+    total = parsed_page.css('div.searchCountContainer').text.split(' ')[3].gsub(',', '').to_i # 6484
+    last_page = (total.to_f / per_page.to_f).round # 432
+    while page <= last_page
+      # pagination_url = "https://www.indeed.com/jobs?q=software+developer&l=Remote&start=#{page}"
+      pagination_url = "https://www.indeed.com/jobs?q=&l=Remote&start=#{page}"
+
+      puts pagination_url
+      puts "Page: #{page}"
+      puts ''
+      pagination_unparsed_page = HTTParty.get(pagination_url)
+      pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page.body)
+      pagination_job_listings = pagination_parsed_page.css('div.jobsearch-SerpJobCard')
+      pagination_job_listings.each do |job_listing|
+        job = {
+          title: job_listing.css('a.jobtitle').children[0].text.strip
+          # title: job_listing.css('a.jobtitle').text.strip.partition('/n')[0],
+          # company: job_listing.css('span.company').text,
+          # location: job_listing.css('span.location').text,
+          # url: "https://indeed.com" + job_listing.css('a')[0].attributes["href"].value
+        }
+        if arr.all? { |x| job[:title].downcase.split.include?(x) }
+          @titles << (job[:title])
+          # @company << (@job[:company])
+          # @locations << (@job[:location])
+          # @urls << (@job[:url])
         end
-        byebug
-    end
 
-    #scraper
+        jobs << job
+        puts @titles
+      end
+      page += 10
+      # byebug
+    end
+    byebug
+  end
+
+  # scraper
 end
 
-#scraper = Scraper.new
+# scraper = Scraper.new
